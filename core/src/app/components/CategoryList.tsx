01a57fb66847b6558382ca325ca1ccc0fb5ca384
Brian Park <brianpark1208@gmail.com>
2025-09-06 00:38:03 -0700

    change categorylist to useeffect

--- core/src/app/components/CategoryList.tsx
+++ core/src/app/components/CategoryList.tsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import { prisma } from "../../../lib/prisma";
 import CategoryButton from "./CategoryButton";
 
 export const dynamic = "force-dynamic";
 
 export default async function CategoryList() {
-  const categories = await prisma.category.findMany();
+    const [categories, setCategories] = useState<any[]>([]);
+
+    useEffect(() => {
+        fetch("/api/categories")
+        .then((res) => res.json())
+        .then((data) => setCategories(data))
+        .catch(console.error);
+    }, []);
   return (
     <div className="category-link-grid">
       {categories.map((category) => (

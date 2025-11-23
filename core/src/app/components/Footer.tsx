c646cff54dba6adb574a2f35a56d66fe6188538b
Brian Park <brianpark1208@gmail.com>
2025-11-22 22:02:50 -0800

    update footer

--- core/src/app/components/Footer.tsx
+++ core/src/app/components/Footer.tsx
@@ -6,47 +6,24 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="footer-container">
-      <nav className="footer-bar">
-        <div className="links-section">
-          <div className="footer-header">Socials</div>
-          <Link href="/" className="footer-text">
-            Instagram
-          </Link>
-          <Link href="/" className="footer-text">
-            X
-          </Link>
-          <Link href="/" className="footer-text">
-            GitHub
-          </Link>
+      <div className="footer-content">
+        <div className="copyright-disclaimer">
+          <span className="disclaimer-text">
+            Copyright © 2025 Brian Park. All rights reserved.
+          </span>
+          <span className="disclaimer-text">
+            Data is from https://www.wikidata.org/wiki/.
+          </span>
         </div>
 
         <div className="links-section">
-          <div className="footer-header">Contact</div>
-          <Link href="/" className="footer-text">
-            Contact Us
-          </Link>
-        </div>
-
-        <div className="links-section">
-          <div className="footer-header">Legal</div>
-          <Link href="/" className="footer-text">
+          <Link href="/privacy" className="footer-text">
             Privacy Policy
           </Link>
-          <Link href="/" className="footer-text">
+          <Link href="/terms" className="footer-text">
             Terms of Use
           </Link>
         </div>
-      </nav>
-
-      <div className="copyright-disclaimer">
-        <span className="disclaimer-text">
-          {" "}
-          Copyright © 2025 Brian Park. All rights reserved.{" "}
-        </span>
-        <span className="disclaimer-text">
-          {" "}
-          Data is from https://www.wikidata.org/wiki/.
-        </span>
       </div>
     </div>
   );
